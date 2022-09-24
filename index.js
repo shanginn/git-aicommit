1f50e8601d4c8718a19cba1eca043e0a64970d1c
Shangin Nikolai <shanginn@gmail.com>
2022-09-24 14:55:58 +0800

    - add .env to .gitignore - make index.js executable - install dotenv as a dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+require('dotenv').config();
 const {Configuration, OpenAIApi} = require("openai");
 const {execSync} = require("child_process");
 
@@ -13,26 +14,53 @@ try {
   process.exit(1);
 }
 
+if (!process.env.OPENAI_API_KEY) {
+  console.error("Please set OPENAI_API_KEY");
+  process.exit(1);
+}
+
 const diff = execSync(
-    'git diff 2>/dev/null',
+    `git diff -- . ':(exclude)*.lock' 2>/dev/null`,
     {encoding: 'utf8'}
 );
 
-console.log(diff);
-process.exit();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-//
-// const response = await openai.createCompletion({
-//   model: "text-davinci-002",
-//   prompt: "Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nContact the ski store and figure out if I can get my skis fixed before I leave on Thursday",
-//   temperature: 0,
-//   max_tokens: 100,
-//   top_p: 1.0,
-//   frequency_penalty: 0.2,
-//   presence_penalty: 0.0,
-//   stop: ["\n"],
-// });
+const openai = new OpenAIApi(configuration);
+
+// create a prompt
+const prompt = `Read the following git diff for a multiple files:
+
+${diff}
+
+Generate a commit message to explain diff in each file:
+
+`;
+
+openai
+  .createCompletion({
+    prompt,
+    model: "text-davinci-002",
+    max_tokens: 500,
+    temperature: 0.2,
+    top_p: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+    best_of: 1,
+    n: 1,
+    stream: false,
+    stop: ["\n\n\n"],
+  })
+  .then((data) => {
+    const commitMessage = data.data.choices[0].text;
+    const command = `git add --all && git commit -m "${commitMessage}"`;
+
+    if (!process.env.GIT_AUTOCOMMIT) {
+      console.log(`Please set GIT_AUTOCOMMIT to commit with following command:\n ${command}`);
+    } else {
+      execSync(command, {encoding: 'utf8'});
+    }
+  });
+
