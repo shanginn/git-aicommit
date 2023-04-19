a4e81f5e3d93b6f9b7911d5f6876aef69d43d99a
Nikolai Shangin <shanginn@gmail.com>
2023-04-19 10:43:28 +0800

    Refactored and extracted code dealing with text and filenames, merged OpenAI generated changes and generated concise commit messages.

--- autocommit.js
+++ autocommit.js
@@ -1,13 +1,19 @@
-#!/usr/bin/env bun
+#!/usr/bin/env node
 
 import { execSync, spawn } from "child_process";
 import rc from 'rc';
-import { ChatOpenAI } from "langchain/chat_models";
-import {ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate} from "langchain/prompts";
+import {
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate
+} from "langchain/prompts";
 import defaultConfig from './config.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {loadSummarizationChain} from "langchain/chains";
+import {ChatOpenAI} from "langchain/chat_models/openai";
+import {OpenAI} from "langchain/llms/openai";
+import fs from "fs";
 
 const config = rc(
     'git-aicommit',
@@ -35,13 +41,14 @@ if (!config.openAiKey) {
 const excludeFromDiff = config.excludeFromDiff || [];
 const diffFilter = config.diffFilter || 'ACMRTUXB';
 const diffCommand = `git diff --staged \
+    --no-ext-diff \
     --diff-filter=${diffFilter} \
     -- "${excludeFromDiff.map(
         (pattern) => `:(exclude)${pattern}`
     ).join(' ')}"
 `;
 
-const diff = execSync(diffCommand, {encoding: 'utf8'});
+let diff = execSync(diffCommand, {encoding: 'utf8'});
 
 if (!diff) {
     console.error("Diff seems empty. Please commit manually.");
@@ -55,13 +62,65 @@ const openai = new ChatOpenAI({
     maxTokens: config.maxTokens,
 });
 
-const systemMessagePromptTemplate = SystemMessagePromptTemplate.fromTemplate(config.systemMessagePromptTemplate)
-const humanPromptTemplate = HumanMessagePromptTemplate.fromTemplate(config.humanPromptTemplate)
+const systemMessagePromptTemplate = SystemMessagePromptTemplate.fromTemplate(
+    config.systemMessagePromptTemplate
+);
+
+const humanPromptTemplate = HumanMessagePromptTemplate.fromTemplate(
+    config.humanPromptTemplate
+);
 
 const chatPrompt = ChatPromptTemplate.fromPromptMessages([
     systemMessagePromptTemplate,
     humanPromptTemplate,
-])
+]);
+
+if (diff.length > 2000) {
+    const splitDiff = diff.split('diff --git ');
+
+    const filenameRegex = /^a\/(.+?)\s+b\/(.+?)/;
+
+    // extract filenames with content in an object from splitDiff
+    const diffByFiles = splitDiff
+        .filter((fileDiff) => fileDiff.length > 0)
+        .map((fileDiff) => {
+            const match = fileDiff.match(filenameRegex);
+            const filename = match[1] || 'unknown';
+
+            const content = fileDiff
+                .replaceAll(filename, '')
+                .replaceAll('a/ b/\n', '')
+
+            return chatPrompt
+                .formatMessages({
+                    diff: content,
+                    language: config.language,
+                })
+                .then((prompt) => {
+                    return openai.call(prompt)
+                        .then((res) => {
+                            return {
+                                filename: filename,
+                                changes: res.text.trim(),
+                            }
+                        })
+                        .catch((e) => {
+                            console.error(`Error during OpenAI request: ${e.message}`);
+                            process.exit(1);
+                        });
+                });
+        });
+
+    // wait for all promises to resolve
+    const mergeChanges = await Promise.all(diffByFiles);
+
+    diff = mergeChanges
+        .map((fileDiff) => {
+            return `diff --git ${fileDiff.filename}\n${fileDiff.changes}`
+
+        })
+        .join('\n\n')
+}
 
 const prompt = await chatPrompt.formatMessages({
     diff: diff,
@@ -69,6 +128,10 @@ const prompt = await chatPrompt.formatMessages({
 })
 
 const res = await openai.call(prompt)
+    .catch((e) => {
+        console.error(`Error during OpenAI request: ${e.message}`);
+        process.exit(1);
+    });
 
 const commitMessage = res.text.trim();
 
